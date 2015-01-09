993550b6e6103d89af9facfa9f30fef069aa88a3
Graham Cox <graham@grahamcox.co.uk>
2015-01-09 12:44:45 +0000

    Index page that loads on all unhandled routes

---
+++ webapp/src/main/kotlin/uk/co/grahamcox/elloria/IndexController.kt
@@ -0,0 +1,29 @@
+package uk.co.grahamcox.elloria.webapp
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import java.time.Clock
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.servlet.ModelAndView
+
+/**
+ * Controller to get the actual main webapp
+ * @param clock The clock to use
+ */
+[Controller]
+class IndexController(private val clock: Clock) {
+    /**
+     * Get the index page
+     * @return the index page
+     */
+    [RequestMapping(value = array("/**"))]
+    [ResponseBody]
+    fun getIndex(): ModelAndView {
+        val response = ModelAndView("/index")
+        response.addObject("now", ZonedDateTime.now(clock).format(DateTimeFormatter.ISO_ZONED_DATE_TIME))
+        return response
+    }
+
+}
---
+++ webapp/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><spring:message code="page.title" /></title>
+        <!-- <c:out value="${now}" /> -->
+    </head>
+    <body>
+
+    </body>
+</html>
