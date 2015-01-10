277f7d5711ca8f4cee16aabf50f9015fe46917fe
Graham Cox <graham@grahamcox.co.uk>
2015-01-10 15:03:30 +0000

    Include the input javascript in the output artifact as well, just in case, and make sure that the javascript can be loaded via Spring

--- javascript/src/main/javascript/elloria.js
+++ javascript/src/main/javascript/elloria.js
@@ -11,7 +11,7 @@
      * the path will default to be a directory called "build" as a sibling
      * to the build file. All relative paths are relative to the build file.
      */
-    dir: "${project.build.outputDirectory}/javascript",
+    dir: "${project.build.outputDirectory}/javascript/built",
     /*
      *  How to optimize all the JS files in the build output directory.
      *  Right now only the following values
