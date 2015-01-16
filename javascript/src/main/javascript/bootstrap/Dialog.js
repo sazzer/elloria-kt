abce5b5e079232982bc918514119664e012aeebc
Graham Cox <graham@grahamcox.co.uk>
2015-01-16 12:58:31 +0000

    Extracted the bulk of the actual dialog code from the Login Dialog into a reusable component

---
+++ javascript/src/main/javascript/bootstrap/Dialog.js
@@ -0,0 +1,64 @@
+define([], function() {
+    return React.createClass({displayName: 'Dialog',
+        mixins: [ReactIntl.Mixin],
+        propTypes: {
+            'label': React.PropTypes.string.isRequired,
+            'buttons': React.PropTypes.array.isRequired
+        },
+
+        /**
+         * Actually display the dialog on the screen
+         */
+        show: function() {
+            var modalBase = this.refs.modalBase,
+                domNode = modalBase.getDOMNode();
+            $(domNode).modal();
+        },
+        /**
+         * Render the React nodes that represent the dialog
+         */
+        render: function() {
+            var buttons = this.props.buttons.map(function(button) {
+                var label = this.getIntlMessage(button.label);
+                var settings = {
+                    type: 'button', 
+                    className: ['btn']
+                };
+
+                if (button.dismiss) {
+                    settings['data-dismiss'] = 'modal';
+                }
+
+                if (button.defaultButton) {
+                    settings.className.push('btn-default');
+                }
+                if (button.primaryButton) {
+                    settings.className.push('btn-primary');
+                }
+                settings.className = settings.className.join(' ');
+                return React.createElement('button', settings, label);
+            }.bind(this));
+            return React.createElement('div', {className: 'modal fade', ref: 'modalBase'}, [
+                React.createElement('div', {className: 'modal-dialog'}, [
+                    React.createElement('div', {className: 'modal-content'}, [
+                        React.createElement('div', {className: 'modal-header'}, [
+                            React.createElement('button', {
+                                'className': 'close',
+                                'data-dismiss': 'modal',
+                                'aria-label': 'Close'
+                            }, [
+                                React.createElement('span', {'aria-hidden': 'true'}, '×') // Note, this is the &times; entity
+                            ]),
+                            React.createElement('h4', {className: 'modal-title'}, this.getIntlMessage(this.props.label))
+                        ]),
+                        React.createElement('div', {className: 'modal-body'}, this.props.children),
+                        React.createElement('div', {className: 'modal-footer'}, buttons)
+                    ])
+                ])
+            ]);
+        }
+    });
+});
+
+
+
--- javascript/src/main/javascript/ui/login/LoginDialog.js
+++ javascript/src/main/javascript/ui/login/LoginDialog.js
@@ -1,40 +1,24 @@
-define([], function() {
+define(['bootstrap/Dialog'], function(Dialog) {
     return React.createClass({displayName: 'LoginDialog',
         show: function() {
-            var modalBase = this.refs.modalBase,
-                domNode = modalBase.getDOMNode();
-            $(domNode).modal();
+            this.refs.dialog.show();
         },
         render: function() {
-            return React.createElement('div', {className: 'modal fade', ref: 'modalBase'}, [
-                React.createElement('div', {className: 'modal-dialog'}, [
-                    React.createElement('div', {className: 'modal-content'}, [
-                        React.createElement('div', {className: 'modal-header'}, [
-                            React.createElement('button', {
-                                'className': 'close',
-                                'data-dismiss': 'modal',
-                                'aria-label': 'Close'
-                            }, [
-                                React.createElement('span', {'aria-hidden': 'true'}, '&times;')
-                            ]),
-                            React.createElement('h4', {className: 'modal-title'}, 'Modal Title')
-                        ]),
-                        React.createElement('div', {className: 'modal-body'}, [
-                            React.createElement('p', {}, 'One fine body')
-                        ]),
-                        React.createElement('div', {className: 'modal-footer'}, [
-                            React.createElement('button', {
-                                'type': 'button',
-                                'className': 'btn btn-default',
-                                'data-dismiss': 'modal'
-                            }, 'Close'),
-                            React.createElement('button', {
-                                'type': 'button',
-                                'className': 'btn btn-primary'
-                            }, 'Save')
-                        ])
-                    ])
-                ])
+            return React.createElement(Dialog, {
+                ref: 'dialog',
+                label: 'page.header.login.label',
+                buttons: [
+                    {
+                        label: 'loginDialog.buttons.close',
+                        defaultButton: true,
+                        dismiss: true
+                    }, {
+                        label: 'loginDialog.buttons.login',
+                        primaryButton: true
+                    }
+                ]
+            }, [
+                React.createElement('p', {}, 'One fine body')
             ]);
         }
     });
