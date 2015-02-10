3c2525a3a1efc2900f256f05e4215db9f40a8928
Graham Cox <graham@grahamcox.co.uk>
2015-02-10 19:35:30 +0000

    Verification tests now open the Elloria running in Jetty

--- webapp/src/test/kotlin/uk/co/grahamcox/elloria/webapp/cucumber/steps/UiSteps.kt
+++ webapp/src/test/kotlin/uk/co/grahamcox/elloria/webapp/cucumber/steps/UiSteps.kt
@@ -4,18 +4,24 @@ import cucumber.api.java.en.Given
 import org.springframework.beans.factory.annotation.Autowired
 import uk.co.grahamcox.elloria.webapp.cucumber.WebDriverProvider
 import cucumber.api.java.After
+import org.slf4j.LoggerFactory
 
 /**
  * Background steps for controlling the UI
  * @param webDriver the injected Web Driver to use
  */
 class UiSteps(private val webDriver: WebDriverProvider) {
+    private val log = LoggerFactory.getLogger(javaClass<UiSteps>())
+
+    /** The HTTP URL to connect to for the tests */
+    private val testUrl = System.getProperty("url.test")
     /**
      * Open up the application in the browser
      */
     [Given("^I have opened the application$")]
     fun openUi() {
-        webDriver.webDriver.get("http://www.google.com")
+        log.info("Connecting to Elloria Application on ${testUrl}")
+        webDriver.webDriver.get(testUrl)
     }
 
     /**
